b223d88676c81eb54a2013c0457ccf4fc68f9687
unknown <guillermo.rosales@IT-MX00964.globant.com>
2015-03-06 20:19:43 -0600

    Subida la primera vez

---
+++ app/src/main/java/dev/guillermorosales/com/exchange/LoginFragment.java
@@ -0,0 +1,229 @@
+package dev.guillermorosales.com.exchange;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+
+import java.util.Arrays;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link LoginFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link LoginFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LoginFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private UiLifecycleHelper uiHelper;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LoginFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LoginFragment newInstance(String param1, String param2) {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+       /* LoginButton authButton = (LoginButton) getView().findViewById(R.id.authButton);
+        authButton.setReadPermissions(Arrays.asList("public_profile"));*/
+
+
+
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        uiHelper = new UiLifecycleHelper(getActivity(), statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
+        View v=inflater.inflate(R.layout.fragment_login, container, false);
+
+        LoginButton authButton = (LoginButton) v.findViewById(R.id.authButton);
+        authButton.setReadPermissions(Arrays.asList("public_profile","email"));
+        authButton.setFragment(this);
+        authButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+
+            @Override
+
+            public void onUserInfoFetched(GraphUser user) {
+
+                if (user != null) {
+
+                    Log.d("Log in", "Logged in as "+user.getProperty("email"));
+
+                } else {
+
+                    Log.d("Log in", "Logged out");
+
+                }
+
+
+
+
+            }
+
+        });
+
+
+
+        return v;
+    }
+
+    public boolean isLoggedIn() {
+        Session session = Session.getActiveSession();
+        return (session != null && session.isOpened());
+    }
+
+
+    // When session is changed, this method is called from callback method
+    private void onSessionStateChange(Session session, SessionState state,
+                                      Exception exception) {
+
+        // When Session is successfully opened (User logged-in)
+        if (state.isOpened()) {
+            Log.d("Loged in", "Logged in...");
+            // make request to the /me API to get Graph user
+            Request.newMeRequest(session, new Request.GraphUserCallback() {
+
+                // callback after Graph API response with user
+                // object
+                @Override
+                public void onCompleted(GraphUser user, Response response) {
+                    if (user != null) {
+                        // Set view visibility to true
+                       /* otherView.setVisibility(View.VISIBLE);
+                        // Set User name
+                        name.setText("Hello " + user.getName());
+                        // Set Gender
+                        gender.setText("Your Gender: "
+                                + user.getProperty("gender").toString());
+                        location.setText("Your Current Location: "
+                                + user.getLocation().getProperty("name")
+                                .toString());*/
+                    }
+                }
+
+
+            }).executeAsync();
+        } else if (state.isClosed()) {
+           /* Log.i(TAG, "Logged out...");
+            otherView.setVisibility(View.GONE);*/
+        }
+    }
+
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                         Exception exception) {
+            if (state.isOpened()) {
+                Log.d("MainActivity", "Facebook session opened.");
+            } else if (state.isClosed()) {
+                Log.d("MainActivity", "Facebook session closed.");
+            }
+        }
+    };
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onFragmentInteraction(Uri uri);
+    }
+
+}
